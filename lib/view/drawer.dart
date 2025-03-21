3a6cbb53dddae5cc680c0bb34e070df7be97b428
crash285 <jackadam862@gmail.com>
2025-03-21 12:14:01 +0100

    unused import

--- lib/view/drawer.dart
+++ lib/view/drawer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 import 'package:walking/local_storage.dart';
 
 class InfoDrawer extends StatelessWidget {
